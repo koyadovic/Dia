283c2c0bc89049520c7bd4c73c5ce7c50d3747b2
Miguel Collado <m.collado.gomez@gmail.com>
2017-06-11 17:06:02 +0200

    We update descriptive models
    Correct the syntax for ABCMeta classes in models.
    
    And we add default constructors for all models.

--- dia/models.py
+++ dia/models.py
@@ -1,9 +1,8 @@
 from abc import ABCMeta
 
 
-
-class DescriptiveModel(ABCMeta):
-    pass
+class DescriptiveModel:
+    __metaclass__ = ABCMeta
 
 
 class Trait(DescriptiveModel):
@@ -12,6 +11,12 @@ class Trait(DescriptiveModel):
     utc_timestamp = None
     kind = None
     value = None
+    def __init__(self, pk=None, user_pk=None, utc_timestamp=None, kind=None, value=None):
+        self.pk = pk
+        self.user_pk = user_pk
+        self.utc_timestamp = utc_timestamp
+        self.kind = kind
+        self.value = value
     
 
 class GlucoseLevel(DescriptiveModel):
@@ -19,6 +24,11 @@ class GlucoseLevel(DescriptiveModel):
     user_pk = None
     utc_timestamp = None
     mgdl_level = None
+    def __init__(self, pk=None, user_pk=None, utc_timestamp=None, mgdl_level=None):
+        self.pk = pk
+        self.user_pk = user_pk
+        self.utc_timestamp = utc_timestamp
+        self.mgdl_level = mgdl_level
 
 
 class Activity(DescriptiveModel):
@@ -27,6 +37,12 @@ class Activity(DescriptiveModel):
     utc_timestamp = None
     intensity = None
     minutes = None
+    def __init__(self, pk=None, user_pk=None, utc_timestamp=None, intensity=None, minutes=None):
+        self.pk = pk
+        self.user_pk = user_pk
+        self.utc_timestamp = utc_timestamp
+        self.intensity = intensity
+        self.minutes = minutes
 
 
 class InsulinAdministration(DescriptiveModel):
@@ -35,6 +51,12 @@ class InsulinAdministration(DescriptiveModel):
     utc_timestamp = None
     insulin_type = None
     insulin_units = None
+    def __init__(self, pk=None, user_pk=None, utc_timestamp=None, insulin_type=None, insulin_units=None):
+        self.pk = pk
+        self.user_pk = user_pk
+        self.utc_timestamp = utc_timestamp
+        self.insulin_type = insulin_type
+        self.insulin_units = insulin_units
 
 
 class Feeding(DescriptiveModel):
@@ -48,18 +70,30 @@ class Feeding(DescriptiveModel):
     fat_gr = None
     fiber_gr = None
     alcohol_gr = None
+    def __init__(self, pk=None, user_pk=None, utc_timestamp=None, total_gr=0, total_ml=0, carb_gr=0, protein_gr=0, fat_gr=0, fiber_gr=0, alcohol_gr=0):
+        self.pk = pk
+        self.user_pk = user_pk
+        self.utc_timestamp = utc_timestamp
+        self.total_gr = total_gr
+        self.total_ml = total_ml
+        self.carb_gr = carb_gr
+        self.protein_gr = protein_gr
+        self.fat_gr = fat_gr
+        self.fiber_gr = fiber_gr
+        self.alcohol_gr = alcohol_gr
 
 
 
 """
 Just enumerations
 """
-class TraitKindSexValue():
+class TraitKindSexValue:
     MALE = 'male'
     FEMALE = 'female'
+    __metaclass__ = ABCMeta
     
 
-class TraitKind():
+class TraitKind:
     BIRTH_UTC_TIMESTAMP = 1 # value accepted is long
     HEIGHT_CM = 2 # value accepted is int
     WEIGHT_KG = 3 # value accepted is float
@@ -67,18 +101,23 @@ class TraitKind():
     ABDOMEN_PERIMETER_CM = 5 # value accepted is int
     WAIST_PERIMETER_CM = 6 # value accepted is int
     SEX = 7 # value accepted is one of TraitKindSexValue
+    __metaclass__ = ABCMeta
 
 
-class ActivityIntensity():
+class ActivityIntensity:
     SOFT = 1 # salir a pasear despacio
     MEDIUM = 2 # caminar fuerte ejercicio
     HIGH = 3 # correr al trote, pesas
     EXTREME = 4 # sprints, HIIT, etc
+    __metaclass__ = ABCMeta
 
 
-class InsulinType():
+class InsulinType:
     RAPID = 0
     SHORT = 1
     INTERMEDIATE = 2
     SLOW = 3
     ULTRA_SLOW = 4
+    __metaclass__ = ABCMeta
+
+
